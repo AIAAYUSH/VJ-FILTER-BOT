fbb140176660dd01ebca7ece7c1efa5da8d56753
AIAAYUSH <98707853+AIAAYUSH@users.noreply.github.com>
2025-01-12 18:13:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,7 +113,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'https://gplinks.com')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'gplinks.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '05f8f6d14e60f99c6f1d68dfc4a192c809019ed3')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/+C3v-dw_6yH45YmQ1')
 
@@ -125,8 +125,8 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://gplinks.com')
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gplinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '05f8f6d14e60f99c6f1d68dfc4a192c809019ed3')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/+C3v-dw_6yH45YmQ1') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
